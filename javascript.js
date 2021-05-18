8303dbbf53204d833d896b334ab94f89a70fa2d4
joseoungmin <71538487+joseoungmin@users.noreply.github.com>
2021-05-18 14:43:10 +0900

    Add files via upload

--- javascript.js
+++ javascript.js
@@ -1,48 +1,55 @@
-      function zz(obj){
-          var abc = obj / 12;
-          var abcd = obj % 12;
-          if (obj>=13){writeend.innerHTML =(Math.floor(abc)+'년 '+abcd+'개월 후 목표 달성');}
-          else if (obj ==12){writeend.innerHTML =(Math.floor(abc)+'년 후 목표 달성');}
-          else if  (obj < 12){
-            writeend.innerHTML =(abcd+'개월 후 목표 달성');}
-          }
 
-          function xx(obj){
-              var abc = obj / 4.348;
-              var abcd = obj % 4.348;
-              var a = abc / 12;
-              var b = abc % 12;
-
-              if (obj >= 49){writeend.innerHTML
-                =(Math.floor(a)+'년 '+Math.floor(b)+'개월 '+Math.floor(abcd)+'주 후 목표 달성');}
-              else if (obj==48){writeend.innerHTML
-                =(Math.floor(a)+'년 후 목표 달성');}
-              else if (obj < 4){writeend.innerHTML
-                  =(Math.floor(abcd)+'주 후 목표 달성');}
-              else if(obj < 48){writeend.innerHTML
-                =(Math.floor(b)+'개월 '+Math.floor(abcd)+'주 후 목표 달성');}
-              }
-
-              function cx(obj){
-                  var c = obj / 7;
-                  var d = obj % 7;
-                  var abc = c / 4.348;
-                  var abcd = c % 4.348;
-                  var a = abc / 12;
-                  var b = abc % 12;
-
-                  if (obj >= 366){writeend.innerHTML
-                    =(Math.floor(a)+'년 '+Math.floor(b)+'개월 '+Math.floor(abcd)+'주 후 목표 달성');}
-                  else if (obj==365){writeend.innerHTML
-                    =(Math.floor(a)+'년 후 목표 달성');}
-                  else if (obj < 7){writeend.innerHTML
-                      =(d+'일 후 목표 달성');}
-                  else if(obj < 28){writeend.innerHTML
-                      =(Math.floor(abcd)+'주 '+d+'일 후 목표 달성');}
-                  else if(obj < 365){writeend.innerHTML
-                    =(+Math.floor(b)+'개월 '+Math.floor(abcd)+'주 '+d+'일 후 목표 달성');}
-                  }
-
+function zzzz(obj){
+        var a = obj / 12; //년
+        var b = obj % 12; //월
+        writeend.innerHTML = "";
+        if (a >= 1)
+        {writeend.innerHTML+=(Math.floor(a)+'년 ');
+      }else {writeend.innerHTML += "";}
+      if (b >= 1)
+      {writeend.innerHTML+=(Math.floor(b)+'개월 ');
+    }else {writeend.innerHTML += "";}
+        writeend.innerHTML += " 후 목표달성";
+    }
+function zzz(obj){
+        var abc = obj / 4;
+        var abcd = obj % 4; //주
+        var a = abc / 12; //년
+        var b = abc % 12; //월
+        writeend.innerHTML = "";
+        if (a >= 1)
+        {writeend.innerHTML+=(Math.floor(a)+'년 ');
+      }else {writeend.innerHTML += "";}
+      if (b >= 1)
+      {writeend.innerHTML+=(Math.floor(b)+'개월 ');
+    }else {writeend.innerHTML += "";}
+      if (abcd >= 1)
+      {writeend.innerHTML+=(Math.floor(abcd)+'주');
+    }else {writeend.innerHTML += "";}
+        writeend.innerHTML += " 후 목표달성";
+    }
+    function zz(obj){
+            var d = obj / 7;
+            var c = obj % 7; //일
+            var abc = d / 4.348;
+            var abcd = d % 4.348; //주
+            var a = abc / 12; //년
+            var b = abc % 12; //월
+            writeend.innerHTML = "";
+            if (a >= 1)
+            {writeend.innerHTML+=(Math.floor(a)+'년 ');
+          }else {writeend.innerHTML += "";}
+          if (b >= 1)
+          {writeend.innerHTML+=(Math.floor(b)+'개월 ');
+        }else {writeend.innerHTML += "";}
+          if (abcd >= 1)
+          {writeend.innerHTML+=(Math.floor(abcd)+'주 ');
+        }else {writeend.innerHTML += "";}
+          if (c >= 1)
+            {writeend.innerHTML+=(Math.floor(c)+'일');}
+          else{writeend.innerHTML += "";}
+            writeend.innerHTML += " 후 목표달성";
+        }
 
 
       function fun1(){
@@ -81,11 +88,8 @@
                   write.innerHTML +=(z+"년 "+comma(Math.floor(aa))+"원<br>")
                   aa = aa * cb
                   aa = aa + ab}
-                  z = z + 1
-                  write.innerHTML +=(z+"년 "+comma(Math.floor(aa))+"원<br>")
-                  aa = aa * cb
-                  aa = aa + ab
-                  writeend.innerHTML=(z+"년 후 목표달성")
+                  writeend.innerHTML = "";
+                  writeend.innerHTML+=((z)+'년 후 목표달성');
                   sub.value = "다시 계산하기";
       }else if(bbbbb=="매달"){
                   z = z + 1
@@ -97,11 +101,7 @@
                   write.innerHTML += (z+"개월 "+comma(Math.floor(aa))+"원<br>")
                   aa = aa * cq
                   aa = aa + ab}
-                  z = z + 1
-                write.innerHTML += (z+"개월 "+comma(Math.floor(aa))+"원<br>")
-                  aa = aa * cq
-                  aa = aa + ab
-                  zz(z);
+                  zzzz(z);
                   sub.value = "다시 계산하기";
       }else if(bbbbb=="매주"){
                 z = z + 1
@@ -113,11 +113,7 @@
                 write.innerHTML += (z+"주 "+comma(Math.floor(aa))+"원<br>")
                   aa = aa * cw
                   aa = aa + ab}
-                  z = z + 1
-                write.innerHTML += (z+"주 "+comma(Math.floor(aa))+"원<br>")
-                  aa = aa * cw
-                  aa = aa + ab
-                  xx(z);
+                  zzz(z);
                   sub.value = "다시 계산하기";
       }else if(bbbbb=="매일"){
                 z = z + 1
@@ -129,11 +125,7 @@
                 write.innerHTML += (z+"일 "+comma(Math.floor(aa))+"원<br>")
                   aa = aa * ce
                   aa = aa + ab}
-                  z = z + 1
-                write.innerHTML += (z+"일 "+comma(Math.floor(aa))+"원<br>")
-                  aa = aa * ce
-                  aa = aa + ab
-                  cx(z);
+                  zz(z);
                   sub.value = "다시 계산하기";
       }}
 
@@ -149,5 +141,4 @@
 
     function uncomma(str) {
         str = String(str);
-        return str.replace(/[^\d]+/g, "");
-    }
+        return str.replace(/[^\d]+/g, "");}
